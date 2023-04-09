8d6c7f4f606912b93fb1041acf71c50ea8597b3c
Bartosz Bem (bemolx) <bartosz.bem@bemideas.pl>
2023-04-09 22:48:40 +0200

    fix(callSoap): fix Content-Type header

--- src/utils/callSoap.ts
+++ src/utils/callSoap.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const callSoap = async (xmls: string) =>
   await axios.post<string>(process.env.ZKM_SOAP_URL!, xmls, {
     headers: {
-      "Content-Type": "text/xml",
+      "Content-Type": "text/xml; charset=utf-8",
       Cookie: (Math.random() + 1).toString(36),
     },
   });
