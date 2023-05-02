8f3fafbabb852bb7b95f0241f66513702cabb347
Bartosz Bem (bemolx) <bartosz.bem@bemideas.pl>
2023-05-02 12:44:48 +0200

    Revert "feat: add version route"
    This reverts commit 080e7931dcf438d005ca4a4cd66bbfd7b4431272.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,6 @@ import { getTimetable } from "../controllers/getTimetable/timetable";
 import { getSchedule } from "../controllers/getSchedule/schedule";
 import { getRoute } from "../controllers/getRoute/route";
 import { getNearbyStops } from "../controllers/getNearbyStops/nearby";
-import { getCurrentVersion } from "../controllers/getCurrentVersion/version";
 
 const router = express.Router();
 
@@ -17,6 +16,5 @@ router.get("/timetable", getTimetable);
 router.get("/schedule", getSchedule);
 router.get("/route", getRoute);
 router.get("/nearby", getNearbyStops);
-router.get("/version", getCurrentVersion);
 
 export = router;
